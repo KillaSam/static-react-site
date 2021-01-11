7212279d8fd7fff50d10aa03fe2da6641bd80f77
Sam <plush.semen@gmail.com>
2021-01-11 21:46:04 +0200

    second ver

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
-        display: `minimal-ui`,
+        display: `standalone`,
         icon: `src/images/logo.png`, // This path is relative to the root of the site.
       },
     },
